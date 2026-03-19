09c47c540189c23dfdf375fce8f1ade8e4fc3493
GaurabCodeLab <kumargaurab56@gmail.com>
2026-03-19 16:10:31 +0530

    constants file changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,5 @@
-// export const API_BASE_URL = "https://node-api-nkxo.onrender.com";
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "https://node-api-nkxo.onrender.com";
+// export const API_BASE_URL = "http://localhost:8080";
+
+// export const API_BASE_URL =
+//   location.hostname === "localhost" ? "http://localhost:8080" : "/api";
